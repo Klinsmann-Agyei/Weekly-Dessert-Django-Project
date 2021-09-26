42c31494a9ca0e5c74f8eb78e83acc8b8df48cae
Klinsmann-Agyei <87317341+Klinsmann-Agyei@users.noreply.github.com>
2021-09-26 10:22:12 +0000

    Weekly Dessert Django Project
    Username: klins
    Password: amalitech

---
+++ cafeteria/weeklydessert/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from django.http import Http404
+from django.urls import reverse
+from .models import Week, Choice
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+
+# Add login_required decorator:
+@login_required
+def index(request):
+  latest_week_poll = Week.objects.get(pk=1)
+  context = {
+     "name": request.user,
+    "week_poll": latest_week_poll
+  }
+  return render(request, "index.html", context)
+
+class SignUp(CreateView):
+  form_class = UserCreationForm
+  template_name = "registration/signup.html"
+  success_url = reverse_lazy("login")
+
+class DetailsView(LoginRequiredMixin, DetailView):
+  model = Week
+  template_name = "detail.html"
+
+class ResultsView(LoginRequiredMixin, DetailView):
+  model = Week
+  template_name = "results.html"
+
+def logout_request(request):
+  logout(request)
+  return redirect("index")
+
+
+# Add login_required decorator:
+@login_required
+def vote(request, week_id):
+  try:
+    week = Week.objects.get(pk=week_id)
+    selected_choice = week.choice_set.get(pk=request.POST["choice"])
+  except Week.DoesNotExist:
+    raise Http404("Poll for said week does not exists")
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return redirect("results", week_id)
+
+
+
